6330bbece967aa1719fda7a15b4fc417c930c1bd
abdelrahman <abdelrahman.ellewaa@blink22.com>
2016-09-08 11:59:06 +0200

    commit bs23

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /products
+  # GET /productsssssssssssss
   # GET /products.json
   def index
     @products = Product.all
